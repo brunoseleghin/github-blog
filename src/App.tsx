580788895dbba7616df2f3a4c6d8b5765b388714
brunoseleghin <brunoseleghin@gmail.com>
2024-03-03 20:33:25 -0300

    created Home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
+import { Home } from './pages/Home'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <h1>Github Blog</h1>
+      <Home />
     </ThemeProvider>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { HomeContainer } from './styles'
+import { Header } from '../../components/Header'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <Header />
+
+      <h1>Home</h1>
+    </HomeContainer>
+  )
+}
