24725a2247691d52ac5dc80b071ed2d7a345135a
brunoseleghin <brunoseleghin@gmail.com>
2024-03-03 23:00:12 -0300

    created profile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,92 @@
-import { HomeContainer } from './styles'
+import {
+  HomeContainer,
+  Name,
+  NameContainer,
+  Profile,
+  Avatar,
+  Bio,
+  Text,
+  Info,
+} from './styles'
+
 import { Header } from '../../components/Header'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faArrowUpRightFromSquare,
+  faBuilding,
+  faUserGroup,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { useTheme } from 'styled-components'
+
 export function Home() {
+  const theme = useTheme()
+
   return (
     <HomeContainer>
       <Header />
 
-      <h1>Home</h1>
+      <main>
+        <Profile>
+          <Avatar src="https://github.com/brunoseleghin.png" />
+
+          <div>
+            <NameContainer>
+              <Name>Bruno Seleghin</Name>
+              <div>
+                <a
+                  href="https://github.com/brunoseleghin"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <FontAwesomeIcon
+                  icon={faArrowUpRightFromSquare}
+                  color={theme.blue}
+                  fontSize={12}
+                />
+              </div>
+            </NameContainer>
+
+            <Bio>
+              {`I'm passionate about technology and programming. I always like to
+              feel that I'm evolving, for this reason I always seek to be
+              learning something. 👨‍💻`}
+            </Bio>
+
+            <Info>
+              <div>
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  color={theme['base-label']}
+                  fontSize={18}
+                />
+                <Text>brunoseleghin</Text>
+              </div>
+
+              <div>
+                <FontAwesomeIcon
+                  icon={faBuilding}
+                  color={theme['base-label']}
+                  fontSize={18}
+                />
+                <Text>Totvs</Text>
+              </div>
+
+              <div>
+                <FontAwesomeIcon
+                  icon={faUserGroup}
+                  color={theme['base-label']}
+                  fontSize={18}
+                />
+                <Text>32 seguidores</Text>
+              </div>
+            </Info>
+          </div>
+        </Profile>
+      </main>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,6 +1,80 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.main`
+export const HomeContainer = styled.div`
   max-width: 1440px;
   margin: 0 auto;
+
+  main {
+    margin-top: -85px;
+    display: flex;
+  }
+`
+
+export const Profile = styled.div`
+  position: relative;
+  display: flex;
+  gap: 32px;
+  margin: 0 auto;
+  width: 864px;
+  padding: 32px 40px;
+  background: ${({ theme }) => theme['base-profile']};
+  border-radius: 10px;
+`
+
+export const Avatar = styled.img`
+  height: 148px;
+  width: 148px;
+  border-radius: 100px;
+`
+
+export const NameContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 8px;
+
+  a {
+    font-size: 12px;
+    font-weight: bold;
+    line-height: 160%;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.blue};
+    text-decoration: none;
+    margin-right: 8px;
+  }
+`
+
+export const Name = styled.p`
+  font-family: 'Nunito', sans-serif;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme['base-title']};
+  font-weight: bold;
+  line-height: 160%;
+`
+
+export const Bio = styled.p`
+  font-family: 'Nunito', sans-serif;
+  font-size: 1rem;
+  color: ${({ theme }) => theme['base-text']};
+  font-weight: normal;
+  line-height: 160%;
+  margin-bottom: 24px;
+`
+
+export const Info = styled.div`
+  display: flex;
+  gap: 24px;
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+`
+
+export const Text = styled.p`
+  font-family: 'Nunito', sans-serif;
+  font-size: 1rem;
+  color: ${({ theme }) => theme['base-subtitle']};
+  font-weight: normal;
+  line-height: 160%;
 `
