0ee9f8045cb27d136f68dd38fd482e373413b0d9
brunoseleghin <brunoseleghin@gmail.com>
2024-03-15 20:21:05 -0300

    added token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
+import { token } from '../token'
 
 export const api = axios.create({
   baseURL: 'https://api.github.com',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 })
