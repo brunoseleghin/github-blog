663159bfa8d8b97f1099558487cdcd14b06e102e
brunoseleghin <brunoseleghin@gmail.com>
2024-03-17 12:32:27 -0300

    changed navigate type

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,7 +1,7 @@
 import { formatDistance } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import Markdown from 'react-markdown'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { PostCardContainer, Title, Created, Resume } from './styles'
 
@@ -16,22 +16,27 @@ interface PostCardProps {
 }
 
 export function PostCard({ data }: PostCardProps) {
+  const navigate = useNavigate()
+
   const createdAt = formatDistance(data.created_at, new Date(), {
     addSuffix: true,
     locale: ptBR,
   })
 
+  function handleNavigation() {
+    navigate(`/post/${data.number}`)
+  }
+
   return (
-    <PostCardContainer>
-      <Link to={`/post/${data.number}`}>
-        <div>
-          <Title>{data.title}</Title>
-          <Created>{createdAt}</Created>
-        </div>
-        <Resume>
-          <Markdown>{data.body}</Markdown>
-        </Resume>
-      </Link>
+    <PostCardContainer onClick={handleNavigation}>
+      <div>
+        <Title>{data.title}</Title>
+        <Created>{createdAt}</Created>
+      </div>
+
+      <Resume>
+        <Markdown>{data.body}</Markdown>
+      </Resume>
     </PostCardContainer>
   )
 }
